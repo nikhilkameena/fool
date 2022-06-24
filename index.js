32fae972010d59917a29eb92c6fdcb5324bd54e7
nikhilkameena <102842078+nikhilkameena@users.noreply.github.com>
2022-06-24 16:14:57 +0530

    fool

---
+++ index.js
@@ -0,0 +1,14 @@
+// first
+
+// let controller = new ScrollMagic.Controller();
+// let timeline = new TimelineMax();
+
+// timeline.to(".mainBoxLeft", 3, { left: 0 });
+
+// let scene = new ScrollMagic.Scene({
+//   triggerElement: "#sec",
+//   duration: "100%",
+//   triggerHook: "onEnter",
+// })
+//   .setTween(timeline)
+//   .addTo(controller);
