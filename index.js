c1ac7a192c58f6a46e09ff79cd13b17b10072287
nikhilkameena <102842078+nikhilkameena@users.noreply.github.com>
2025-03-07 16:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@
 
 // timeline.to(".mainBoxLeft", 3, { left: 0 });
 
-// let scene = new ScrollMagic.Scene({
+// let scene = new ScrollMagic.Scene({async innerText(options) {
+    return await this._frame.innerText(this._selector, {
+            strict: true,
+            ...options
+    });
+}Hello, world!
 //   triggerElement: "#sec",
 //   duration: "100%",
 //   triggerHook: "onEnter",
